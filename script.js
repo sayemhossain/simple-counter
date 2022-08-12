5f00f5b637ec9818faf3d974b748aeb3da735c17
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-08-13 03:00:11 +0600

    new code

--- script.js
+++ script.js
@@ -3,15 +3,42 @@ const counterEl = document.getElementById("counter");
 let incrementEl = document.getElementById("increment");
 let decrementEl = document.getElementById("decrement");
 
-// initial state
-let count = 0;
+//initial state
+const initialState = { value: 0 };
 
-// adding event listener
+//Create Reducer function
+function counterReducer(state = initialState, action) {
+  if (action.type === "increment") {
+    return {
+      ...state,
+      value: state.value + 1,
+    };
+  } else if (action.type === "decrement") {
+    return {
+      ...state,
+      value: state.value - 1,
+    };
+  } else {
+    return state;
+  }
+}
+
+//create store
+const store = Redux.createStore(counterReducer);
+
+const render = () => {
+  const state = store.getState();
+  counterEl.innerText = state.value.toString();
+};
+store.subscribe(render);
+//button click listen
 incrementEl.addEventListener("click", () => {
-  count++;
-  counterEl.innerText = count;
+  store.dispatch({
+    type: "increment",
+  });
 });
 decrementEl.addEventListener("click", () => {
-  count--;
-  counterEl.innerText = count;
+  store.dispatch({
+    type: "decrement",
+  });
 });
