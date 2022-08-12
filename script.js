45a75764e9a24bb8a598aae57b6d52a2e5a75c9a
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-08-13 01:03:54 +0600

    new code

---
+++ script.js
@@ -0,0 +1,4 @@
+// select dom element
+const counterEl = document.getElementById("counter");
+const incrementEl = document.getElementById("increment");
+const decrementEl = document.getElementById("decrement");
